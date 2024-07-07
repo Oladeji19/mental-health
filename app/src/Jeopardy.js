de76fc392a5f05ad17317243e1094cb2310172b0
sakura16 <supermariosonic20@gmail.com>
2024-07-07 15:01:25 -0400

    Create Jeopardy.js

---
+++ app/src/Jeopardy.js
@@ -0,0 +1,54 @@
+import "./App.css";
+
+function Jeopardy(){
+    return(
+    <div className="jeopardy">
+        Jeopardy Game
+        <div className="our-mission">
+            <div className="jeopardy-msg">
+                <h1 className="title1">Jeopardy</h1>
+                <h2 className="subtitle1">Types of Mental Health Disorders</h2>
+                    <h3><a href="1a.html">$200</a></h3>
+                    <h3><a href="1b.html">$400</a></h3>
+                    <h3><a href="1c.html">$600</a></h3>
+                    <h3><a href="1d.html">$800</a></h3>
+                    <h3><a href="1e.html">$1000</a></h3>
+                <h2 className="subtitle2">Famous Figures and Mental Health</h2>
+                    <h3><a href="2a.html">$200</a></h3>
+                    <h3><a href="2b.html">$400</a></h3>
+                    <h3><a href="2c.html">$600</a></h3>
+                    <h3><a href="2d.html">$800</a></h3>
+                    <h3><a href="2e.html">$1000</a></h3>
+                <h2 className="subtitle3">Symptoms and Signs</h2>
+                    <h3><a href="3a.html">$200</a></h3>
+                    <h3><a href="3b.html">$400</a></h3>
+                    <h3><a href="3c.html">$600</a></h3>
+                    <h3><a href="3d.html">$800</a></h3>
+                    <h3><a href="3e.html">$1000</a></h3>
+                <h2 className="subtitle4">Coping Strategies</h2>
+                    <h3><a href="4a.html">$200</a></h3>
+                    <h3><a href="4b.html">$400</a></h3>
+                    <h3><a href="4c.html">$600</a></h3>
+                    <h3><a href="4d.html">$800</a></h3>
+                    <h3><a href="4e.html">$1000</a></h3>
+                <h2 className="subtitle5">Therapies and Treatments</h2>
+                    <h3><a href="5a.html">$200</a></h3>
+                    <h3><a href="5b.html">$400</a></h3>
+                    <h3><a href="5c.html">$600</a></h3>
+                    <h3><a href="5d.html">$800</a></h3>
+                    <h3><a href="5e.html">$1000</a></h3>
+                <h2 className="subtitle6">Mental Health Myths and Facts</h2>
+                    <h3><a href="6a.html">$200</a></h3>
+                    <h3><a href="6b.html">$400</a></h3>
+                    <h3><a href="6c.html">$600</a></h3>
+                    <h3><a href="6d.html">$800</a></h3>
+                    <h3><a href="6e.html">$1000</a></h3>
+
+            </div>
+        </div>
+    </div>
+
+    )
+}
+
+export default Jeopardy;
