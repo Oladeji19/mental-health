92768e1aaf6e9092c479449003d90f7a4e1be650
sakura16 <supermariosonic20@gmail.com>
2024-07-14 12:30:13 -0400

    Update NavBar.js

--- app/src/NavBar.js
+++ app/src/NavBar.js
@@ -1,17 +1,32 @@
 import "./App.css";
+import App from "./App.js";
+import Mindfulness from "./Mindfulness.js";
+import {useState} from "react";
 import logo from "./assets/mindful-logo.png";
 
 function NavBar() {
+  const [page, setPage] = useState({data:<App />});
+  const mindfulnessPage = ()=>{
+    setPage({...page,data:<Mindfulness/>})
+  };
   return (
+    <>
     <div className="nav-bar">
       <img className="logo" src={logo} />
       <div className="options">
+        <div className="mindfulness" onClick={mindfulnessPage}>Mindfulness</div>
         <div className="activities">Activities</div>
         <div className="suggestions">Suggestions</div>
         <div className="login-button">Log In</div>
       </div>
     </div>
+    <div className="current-page">
+        {page.data}
+    </div>
+    </>
   );
 }
+
+
 
 export default NavBar;
