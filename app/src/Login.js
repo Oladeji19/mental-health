953af2704a2a052b1debce4d0caa52dce5606bd6
keshavs1818 <103164532+keshavs1818@users.noreply.github.com>
2024-07-23 18:30:34 -0400

    Update Login.js

--- app/src/Login.js
+++ app/src/Login.js
@@ -1,8 +1,14 @@
 function Login() {
    return (
-     <div>
-        {/*Login content begins */}
-        <h1>Hi</h1>
+     <div className="captchabackground">
+         <h3>I am not a robot</h3>
+         <canvas id="captcha">captcha text</canvas>
+         <input id="textBox" type="text" name="text"/>
+            <div id="buttons">
+               <input id="submitButton" type="submit"/>
+               <button id="refreshButton" type="submit">Refresh</button>
+            </div>
+         <span id="output"></span>
      </div>
    );
 }
