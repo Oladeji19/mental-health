03d3fedbdcd57b75dc6a352eb929e8d6bc597e2e
keshavs1818 <103164532+keshavs1818@users.noreply.github.com>
2024-08-10 16:30:09 -0400

    Update Suggestions.js

--- app/src/Suggestions.js
+++ app/src/Suggestions.js
@@ -2,7 +2,13 @@ function Suggestions() {
     return (
         <div>
             {/*Suggestions content begins */}
-            <h1>Test</h1>
+            <h1 className="recommendations">Recommendation System</h1>
+            {/* Gives a description for the recommendation system. */}
+            <p className="recommendation-desc">
+                We want to help improve your mental health by finding resources for you
+                such as YouTube videos, websites, books, nearby events you may enjoy, or
+                a safe space to talk to someone.
+            </p>
         </div>
     );
 }
