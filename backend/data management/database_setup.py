ccc6b4db7e850698882d4035bec07e50c221083a
pythoncoder1999 <155390850+pythoncoder1999@users.noreply.github.com>
2024-08-11 18:08:27 -0400

    Update database_setup.py
    Database contains 2-factor verification data

--- backend/data management/database_setup.py
+++ backend/data management/database_setup.py
@@ -11,7 +11,16 @@ def create_tables():
     try:
         conn = create_connection()
         cursor = conn.cursor()
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS current_verification_requests(
+                        username TEXT NOT NULL UNIQUE,
+                        code_sent INTEGER,
+                        time_sent TEXT,
+                        user_type TEXT
         
+        )
+        ''')
         # Create the problem table
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS problem (
@@ -20,7 +29,7 @@ def create_tables():
         )
         ''')
         
-        # Create the listener table. When the listeners, registers they select their problem specialization, availability, language and experience level
+        # Create the listener table. When the listeners, registers they select their problem specialization, availability, language, if they have 2-factor authentication and experience level
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS listener (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,6 +39,7 @@ def create_tables():
             language TEXT NOT NULL,
             experience_level TEXT NOT NULL,
             availability TEXT DEFAULT 'unavailable'
+            has_2_factor INTEGER DEFAULT 0
         )
         ''')
 
@@ -44,13 +54,14 @@ def create_tables():
         )
         ''')
         
-        # Create the user table. This table stores the user's email, username and password
+        # Create the user table. This table stores the user's email, username, password, and if they have 2-factor authentication
         cursor.execute('''
         CREATE TABLE IF NOT EXISTS user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             email TEXT NOT NULL UNIQUE,
             username TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL
+            has_2_factor INTEGER DEFAULT 0
         )
         ''')
         
