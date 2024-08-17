3c4f13f9328ca073433f196b63217c0dabd67002
nathan-lang <76454416+nathan-lang@users.noreply.github.com>
2024-08-17 14:57:35 -0400

    Adding node_modules and resetting routing feature in Login Page.

--- app/src/Journal.js
+++ app/src/Journal.js
@@ -1,85 +1,99 @@
 import { useState } from "react";
 
 function Journal() {
-   {/* Created state variables for array as well as input_val */}
-   const [array, setArr] = useState([]);
-   const [input_val, setInputVal] = useState("");
+  {
+    /* Created state variables for array as well as input_val */
+  }
+  const [array, setArr] = useState([]);
+  const [input_val, setInputVal] = useState("");
 
-   {/* Sets the input for the text area and saves the textbox for adding. */}
-   const handleInputChange = (e) => {
+  {
+    /* Sets the input for the text area and saves the textbox for adding. */
+  }
+  const handleInputChange = (e) => {
     setInputVal(e.target.value);
     console.log(e.target.value);
+  };
 
-   };
-
-   {/* Upon submitting the thought, it gets added to the main page. */}
-   const handleSubmit = (e) => {
+  {
+    /* Upon submitting the thought, it gets added to the main page. */
+  }
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setArr(oldArray=>[input_val, ...oldArray]);
+    setArr((oldArray) => [input_val, ...oldArray]);
     console.log(array);
-   };
+  };
 
-   {/* Upon the click of the journal edit button, it allows you to edit the entry in the journal page. */}
-   const editEntry = (index) => {
-      console.log(index);
-      let newEntry = prompt("Edit your entry:");
-      const newEntries = [...array];
-      newEntries[index] = newEntry;
+  {
+    /* Upon the click of the journal edit button, it allows you to edit the entry in the journal page. */
+  }
+  const editEntry = (index) => {
+    console.log(index);
+    let newEntry = prompt("Edit your entry:");
+    const newEntries = [...array];
+    newEntries[index] = newEntry;
+    setArr(newEntries);
+  };
+
+  {
+    /* Upon the click of the button, the entry in the journal gets deleted. */
+  }
+  const deleteEntry = (index) => {
+    if (window.confirm("Are you sure you want to delete this entry?")) {
+      const newEntries = array.filter((item, i) => i !== index);
       setArr(newEntries);
-   };
+    }
+  };
 
-   {/* Upon the click of the button, the entry in the journal gets deleted. */}
-   const deleteEntry = (index) => {
-     if(window.confirm("Are you sure you want to delete this entry?")) {
-       const newEntries = array.filter((item, i) => i !== index);
-       setArr(newEntries);
-     }
-   }
+  {
+    /* Creates state variable for counting characters. */
+  }
+  const [charCount, setCharCount] = useState(0);
 
-   {/* Creates state variable for counting characters. */}
-   const [charCount, setCharCount] = useState(0);
-
-   return (
-      <div className="journal">
-        {/* Journal title is being demonstrated. */}
-        <h1>Journal Time</h1>
-        <h3>Welcome to the journal webpage. Write whatever thoughts you feel like sharing.</h3>
-        {/* Submit button. Adds the thought to the main page upon click. */}
-        <form className="journal-form" onSubmit={handleSubmit}>
-            <textarea 
-               className="journal-input"
-               type="text" 
-               value={input_val} 
-               onChange={(e) => {
-                handleInputChange(e);
-                setCharCount(e.target.value.length);
-               }}
-            ></textarea>
-            {charCount}
-            <br></br>
-            <br></br>
-            <button type="submit">Submit</button>
-        </form>
-        {/* Actually responsible for printing out everything in the array. */}
-        <div className="array-thoughts">
-          {/* Creates an entry along with an edit and delete button. */}
-          {array.map((item, index) => (
-            <div className="thought">
-              <div>Entry #{array.length - index}</div>
-              <hr></hr>
-              <div>{item}</div>
-              <button
-                className="journal-edit-button"
-                onClick={() => editEntry(index)}
-              >
-                Edit
-              </button>
-              <button onClick={() => deleteEntry(index)}>Delete</button>
-            </div>
-          ))}
-        </div>
+  return (
+    <div className="journal">
+      {/* Journal title is being demonstrated. */}
+      <h1>Journal Time</h1>
+      <p>
+        Welcome to the journal webpage. Write whatever thoughts you feel like
+        sharing.
+      </p>
+      {/* Submit button. Adds the thought to the main page upon click. */}
+      <form className="journal-form" onSubmit={handleSubmit}>
+        <textarea
+          className="journal-input"
+          type="text"
+          value={input_val}
+          onChange={(e) => {
+            handleInputChange(e);
+            setCharCount(e.target.value.length);
+          }}
+        ></textarea>
+        {charCount}
+        <br></br>
+        <br></br>
+        <button type="submit">Submit</button>
+      </form>
+      {/* Actually responsible for printing out everything in the array. */}
+      <div className="array-thoughts">
+        {/* Creates an entry along with an edit and delete button. */}
+        {array.map((item, index) => (
+          <div className="thought">
+            <div>Entry #{array.length - index}</div>
+            <hr></hr>
+            <div>{item}</div>
+            <button
+              className="journal-edit-button"
+              onClick={() => editEntry(index)}
+            >
+              Edit
+            </button>
+            <button onClick={() => deleteEntry(index)}>Delete</button>
+          </div>
+        ))}
       </div>
-   );
+    </div>
+  );
 }
 
 export default Journal;
--- app/src/Login.js
+++ app/src/Login.js
@@ -3,87 +3,109 @@ import { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import ForgotPassword from "./ForgotPassword.js";
 import Registration from "./Registration.js";
-import { Outlet, Link } from 'react-router-dom';
+//import { Outlet, Link } from 'react-router-dom';
 
 function Login() {
-   {/* Creates a username and handles the username change. */}
-   const [username, setUsername] = useState("");
-   const handleUserNameChange = (e) => {setUsername(e.target.value)};
+  {
+    /* Creates a username and handles the username change. */
+  }
+  const [username, setUsername] = useState("");
+  const handleUserNameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
-   {/* Creates a password and handles the password change. */}
-   const [password, setPassword] = useState("");
-   const handlePasswordChange = (e) => {setPassword(e.target.value)};
-  
-   {/* Functions created for captcha */}
-   let captchaSet = false;
-   const onCaptchaChange = () => {captchaSet = true; };
-   let captcha;
-   const setCaptchaRef = (ref) => {
-      if(ref) { 
-         captcha = ref;
-      }
-      return captcha;
-   };
+  {
+    /* Creates a password and handles the password change. */
+  }
+  const [password, setPassword] = useState("");
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-   {/* Makes sure that captcha is being done before the submit button is being clicked. If button isn't clicked, alert forced for logging in. */}
-   const canSubmit = () => {
-       if(!captchaSet) {
-         alert("Complete the reCAPTCHA below before logging in.");
-       } else {
-         setUsername(username);
-         setPassword(password);
-         console.log("Your details are", username, password);
-         alert("Logged In.");
-       }
-   };
+  {
+    /* Functions created for captcha */
+  }
+  let captchaSet = false;
+  const onCaptchaChange = () => {
+    captchaSet = true;
+  };
+  let captcha;
+  const setCaptchaRef = (ref) => {
+    if (ref) {
+      captcha = ref;
+    }
+    return captcha;
+  };
 
-   
-   return (
-     <div className="login">
-         {/* Contains the login and password information. Allows user to type username and password.*/}
-         <h1 className="login-title">Log In</h1>
-         <p>Please enter your username and password.</p>
-         <div className="form-input">
-            <label>Username</label>
-            <input type="text" id="username" onChange={handleUserNameChange} />
-            <br></br>
-            <label>Password</label>
-            <input type="password" id="password" onChange={handlePasswordChange} />
-         </div>
-         <br></br>
-         <br></br>
-         {/* The container for people who've forgotten their password. */}
-         <div className="remember-forgot">
-            <div className="remember-me">
-               <input type="checkbox" id="remember" />
-               <label htmlFor="remember">Remember me</label>
-            </div>
-            <div className="forgot">
-               <Link to="/forgot-password">Forgot Password?</Link>
-            </div>
-         </div>
-         {/* Allows you to register as a new user. */}
-         <div className="register">
-         <span>
-            Don't have an account?<Link to="/registration">Register?</Link>
-         </span>
-         </div>
-         {/* I'm not a robot button. */}
-         <ReCAPTCHA
-            sitekey="6Lcr3iMqAAAAAM7Znb8LuOPob6pZoH3RGR6MXePd"
-            onChange={onCaptchaChange}
-            ref={(r) => setCaptchaRef(r)}
-            className="captchabackground" 
-         />
-         {/* Button for submitting and refreshing. */}
-         <div id="buttons">
-            <button id="submitButton" type="submit" onClick={canSubmit}>Submit</button>
-            <button id="refreshButton" type="submit" onClick={() => captcha.reset()}>Refresh</button>
-         </div>
-         {/* Generated output. */}
-         <span id="output"></span>
-     </div>
-   );
+  {
+    /* Makes sure that captcha is being done before the submit button is being clicked. If button isn't clicked, alert forced for logging in. */
+  }
+  const canSubmit = () => {
+    if (!captchaSet) {
+      alert("Complete the reCAPTCHA below before logging in.");
+    } else {
+      setUsername(username);
+      setPassword(password);
+      console.log("Your details are", username, password);
+      alert("Logged In.");
+    }
+  };
+
+  return (
+    <div className="login">
+      {/* Contains the login and password information. Allows user to type username and password.*/}
+      <h1 className="login-title">Log In</h1>
+      <p>Please enter your username and password.</p>
+      <div className="form-input">
+        <label>Username</label>
+        <input type="text" id="username" onChange={handleUserNameChange} />
+        <br></br>
+        <label>Password</label>
+        <input type="password" id="password" onChange={handlePasswordChange} />
+      </div>
+      <br></br>
+      <br></br>
+      {/* The container for people who've forgotten their password. */}
+      <div className="remember-forgot">
+        <div className="remember-me">
+          <input type="checkbox" id="remember" />
+          <label htmlFor="remember">Remember me</label>
+        </div>
+        <div className="forgot">
+          {/* <Link to="/forgot-password">Forgot Password?</Link> */}
+        </div>
+      </div>
+      {/* Allows you to register as a new user. */}
+      <div className="register">
+        <span>
+          Don't have an account?{" "}
+          {/* <Link to="/registration">Register?</Link> */}
+        </span>
+      </div>
+      {/* I'm not a robot button. */}
+      <ReCAPTCHA
+        sitekey="6Lcr3iMqAAAAAM7Znb8LuOPob6pZoH3RGR6MXePd"
+        onChange={onCaptchaChange}
+        ref={(r) => setCaptchaRef(r)}
+        className="captchabackground"
+      />
+      {/* Button for submitting and refreshing. */}
+      <div id="buttons">
+        <button id="submitButton" type="submit" onClick={canSubmit}>
+          Submit
+        </button>
+        <button
+          id="refreshButton"
+          type="submit"
+          onClick={() => captcha.reset()}
+        >
+          Refresh
+        </button>
+      </div>
+      {/* Generated output. */}
+      <span id="output"></span>
+    </div>
+  );
 }
 
 export default Login;
