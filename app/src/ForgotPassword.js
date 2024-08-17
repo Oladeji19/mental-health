fcc0c839b036f33659c20f6f12133d9d5ffd76c8
sakura16 <supermariosonic20@gmail.com>
2024-08-17 14:12:45 -0400

    Update ForgotPassword.js

--- app/src/ForgotPassword.js
+++ app/src/ForgotPassword.js
@@ -2,17 +2,22 @@ import "./App.css";
 import RegistrationPage from "./Registration.js";
 
 function ForgotPassword(){
+    {/* Creates the div for forgetting the password. */}
     <div className="form-input">
+            {/* Title for forgetting the password. */}
             <h1 className="forget-password">Forgot Password</h1>
+            {/* Allows you enter your email as well as a new password. */}
             <h3>Please enter your Email.</h3>
             <label>Email</label>
             <input type="text" id="email" />
             <label>New Password</label>
             <input type="text" id="new-password" />
             <br></br>
+            {/* Confirms the password. */}
             <label>Confirm New Password</label>
             <input type="text" id="confirm-new-password" />
             <br></br>
+            {/* ALlows you to navigate to the registration page. */}
             <div className="register" onClick={RegistrationPage} href="#">
                 Register
             </div>
