d6a225f4a2be84d2d44d0a14b645b3a04a97ac8d
keshavs1818 <103164532+keshavs1818@users.noreply.github.com>
2024-08-28 10:14:16 -0400

    Update ForgotPassword.js

--- app/src/ForgotPassword.js
+++ app/src/ForgotPassword.js
@@ -3,24 +3,44 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 function ForgotPassword() {
+  {/* State variable for the new password created. */}
   const [newPassword, setNewPassword] = useState("");
+
+  {/* State variable for the new password message. Length needs to be at least 12 characters.  */}
   const [newPasswordMsg, setNewPasswordMsg] = useState("Length needs to be at least 12 characters.");
+
+  {/* State variable for the new password message to make sure that it consists of uppercase, lowercase, numbers, and symbols.  */}
   const [newPasswordMsgCharacters, setNewPasswordMsgCharacters] = useState("Needs to contains uppercase and lowercase characters, numbers, and symbols.");
+
+  {/* State variable for the confirmed password.  */}
   const [confirmedPassword, setConfirmedPassword] = useState("");
+
+  {/* State variable to ensure that the confirmed password is the same as the new password.  */}
   const [confirmedPasswordMsg, setConfirmedPasswordMsg] = useState("Password should be the same as the original.");
+
+  {/* State variable for enabling and disabling the button to create the new account assuming that the new password as well as the confirmed password are both the same. */}
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
+  {/* Uppercase letters */}
   const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
   'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  {/* Lowercase letters */}
   const lowercase = [  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  {/* Numbers */}
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  {/* Symbols */}
   const symbols = [
     '@', '#', '$', '%', '&', '*', '!', '?', '^', '~', '|', '_', '+', '-', '=', 
     '<', '>', '×', '÷', '√', '∞', '±', '≠', '≈', '∑', '∫', 'π', '∆', '∂',
     '€', '£', '¥', '₹', '₽', '₩', '₫', '₦',
     '.', ',', ';', ':', '\'', '"', '(', ')', '[', ']'
   ];
+
+  {/* Checks to make sure that the password contains an element from an array. */}
   function apply(arr, val) {
     for(let a of arr) {
       if(val.includes(a)) {
@@ -29,20 +49,26 @@ function ForgotPassword() {
     }
     return false;
   }
+  {/* Updates the new password message for character check and length check as and when the user is adding a new password. */}
   const handlePasswordChange = (event) => {
+    {/* Takes in the value. */}
     const value = event.target.value;
+    {/* Apply function is created for symbols, uppercase, lowercase and numbers. For each apply function, a check happens to make sure that at least one of each: uppercase, lowercase, symbol, and numbers are present. */}
     const booleans = [apply(symbols, value), apply(uppercase, value), apply(lowercase, value), apply(numbers, value)]
+    {/* Setting the new password, the new password length check message, as well as the new password character check message. */}
     setNewPassword(value);
     setNewPasswordMsg(value.length < 12 ? "Length needs to be at least 12 characters." : "");
     setNewPasswordMsgCharacters(booleans.includes(false) ? "Needs to contains uppercase and lowercase characters, numbers, and symbols." : "");
   }
   const handleConfPasswordChange = (event) => {
+    {/* Takes in the value. */}
     const value = event.target.value;
-    const booleans = [apply(symbols, value), apply(uppercase, value), apply(lowercase, value), apply(numbers, value)]
+    {/* Sets the confirmed password and message depending on whether the confirm password is the same as the new password. */}
     setConfirmedPassword(value);
-    setConfirmedPasswordMsg(value != newPassword ? "Password should be the same as the original." : "All set. Have fun.");
-    const passwordBools = [newPasswordMsg == "", confirmedPasswordMsg == "All set. Have fun.", newPasswordMsgCharacters == ""];
-    setIsButtonDisabled(passwordBools.includes(false));
+    setConfirmedPasswordMsg(value !== newPassword ? "Password should be the same as the original." : "All set. Have fun.");
+    console.log(confirmedPasswordMsg !== "All set. Have fun.");
+    {/* Enables button if message says "All set. Have fun.". Else, keep the button disabled. */}
+    setIsButtonDisabled(confirmedPasswordMsg !== "All set. Have fun.");
   }
   return (
     <div className="form-input">
@@ -51,13 +77,15 @@ function ForgotPassword() {
       {/* Allows you enter your email as well as a new password. */}
       <p>Please enter your email and new password.</p>
       <label>Email</label>
-      <input type="text" id="email" />
+      <input type="text" id="email" placeholder="Enter your email:"/>
       <br></br>
       <label>New Password</label>
       <input type="text" value={newPassword} onChange={handlePasswordChange} placeholder="Enter new password"/>
       <br></br>
-      <p>{newPasswordMsg}</p>
-      <p>{newPasswordMsgCharacters}</p>
+      <div className="new-pass">
+        <p>{newPasswordMsg}</p>
+        <p>{newPasswordMsgCharacters}</p>
+      </div>
       {/* Confirms the password. */}
       <label>Confirm New Password</label>
       <input type="text" value={confirmedPassword} onChange={handleConfPasswordChange} placeholder="Re-enter password" />
@@ -66,6 +94,7 @@ function ForgotPassword() {
       {/* ALlows you to navigate to the registration page. */}
       <button disabled={isButtonDisabled}>Register</button>
       <br></br>
+      {/* Allows you to navigate back to the login page. */}
       <Link to="/">Click here to go back</Link>
     </div>
   );
